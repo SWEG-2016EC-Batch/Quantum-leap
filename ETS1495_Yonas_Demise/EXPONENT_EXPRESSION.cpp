a77d3a34597c61f37be120efe19cd9dda97ff917
Yonas <yonas.demise@aastustudent.edu.et>
2024-11-29 23:12:13 +0300

    Update EXPONENT_EXPRESSION.cpp

--- ETS1495_Yonas_Demise/EXPONENT_EXPRESSION.cpp
+++ ETS1495_Yonas_Demise/EXPONENT_EXPRESSION.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    double num1,num2,result;
+    char answer;
+    label1:
+    cout<<"Please enter the base number: "<<endl;
+    cin>>num1;
+    if(cin.fail()) {
+        cout<<"The input you entered is not correct, please enter a number only!"<<endl;
+        cin.clear();
+        cin.ignore();
+        goto label1;
+    }
+    label2:
+    cout<<"Please enter the exponent:"<<endl;
+    cin>>num2;
+    if(cin.fail()) {
+        cout<<"The input you entered is not correct, please enter a number only!"<<endl;
+        cin.clear();
+        cin.ignore();
+        goto label2;
+    }
+    result= pow(num1,num2);
+    cout<<"The result is: "<<result<<endl;
+    label3:
+    cout<<"Do you want to calculate again?\n say 'Y' for yes and 'N' for no"<<endl;
+    cin>>answer;
+    if(answer=='Y') {
+        goto label1;
+    } else if (answer=='N') {
+    cout<<"Have a nice day!"<< endl;
+    } else {
+    cout<<"Please say 'Y' for yes and 'N' for no"<<endl;
+    goto label3;
+    }
+}
 
