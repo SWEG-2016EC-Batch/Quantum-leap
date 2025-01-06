4201b3c26ca1b9784ebaf9a4633dd75726badf7a
Yonas Zegeye Tesfaye <yonas.zegeye@aastustudent.edu.et>
2025-01-06 18:25:00 +0300

    Update LMS.cpp

--- Project/LMS.cpp
+++ Project/LMS.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    const int MAX_PATRONS = 1000;  // Maximum number of patrons
+    const int MAX_BOOKS = 1000;  // Maximum number of books
+    int numBooks = 0;
+    int MAX_BORROWINGS = 20, user_id[MAX_PATRONS][MAX_BORROWINGS+1], borrowerId[MAX_PATRONS];  // first column contains id, second one contains the number of books borrowed.      
+    const char correctName[] = "Yoseph Asrat Kebede";
+    const char correctPassword[] = "14526342652";
+    string user_name[MAX_PATRONS], name, sex, address, searchName; // first column of user name has the patron names, and the rest are the books they borrowed based on id.
+    string user_sex[MAX_PATRONS]; 
+    string user_address[MAX_PATRONS], login_password, log_pswd;
+    string book, booksId[MAX_BOOKS][2] = {}, borrowed_books[MAX_BOOKS][2]; // Array of books by name and id.
+    int totalPatrons = 0,errorCounter=0, maximumErrorAttempt=3;        // Count of registered patrons
+    // arrays to store borrowing records for the day
+    int numBorrowings = 0;  // number of borrowings for the day
+    char security_answer[50], security_question[50], attempts = 3;
+    int secure, choose, searchId, id, dueDate = 10, total_penalty = 0, penalty_rate = 10,consumerCount = 0,bookId, add, idd, borrow_count = 0;
+    int uniqueBorrowers = 0;
+    long actionTime[MAX_PATRONS][3] = {};
+    double secondsDifference, daysDifference;
+    int choice;
+    while (true) {
+//Main menu:
+        menu:
+        cout << "\n********Library System Main Menu:********\n";
+        cout << "0. Resigster as the Librarian\n";
+        cout << "1. Login Librarian\n";
+        cout << "2. Register Patron\n";
+        cout << "3. Login Patron\n";
+        cout << "4. Cancel Membership\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin>>choice;
+    
+        // Yonas Z's warning feature
+
+        if (cin.fail() || choice < 0 || choice > 5) {
+            cin.clear();
+            cin.ignore();
+            errorCounter++;
+            if (errorCounter<maximumErrorAttempt) {
+                    cout << "If you are not sure to about exiting.\n";
+                    goto menu;
+            }
+            else{
+                    cout << "Exiting the system. Goodbye!\n";
+                    return 0;
+                }
+
+        }
+
+//----------------------------------end of Yonas Z's feature-----
 
