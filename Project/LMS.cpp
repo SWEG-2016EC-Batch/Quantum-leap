fdc94dce10adbe2dffb9b46c49dc063f7bca5378
Yonas Zegeye Tesfaye <yonas.zegeye@aastustudent.edu.et>
2025-01-06 18:47:50 +0300

    Update LMS.cpp

--- Project/LMS.cpp
+++ Project/LMS.cpp
@@ -83,6 +83,566 @@ int main() {
 
 
 
+//----------------------Yonas Z's feature:
+            cout << "Register yourself as the new Library Manager\n";
+            cout << "Enter your name: ";
+            cin.ignore();
+            getline(cin, name);
+
+            cout << "Enter your sex: ";
+            getline(cin, sex);
+
+            cout << "Enter your address: ";
+            getline(cin, address);
+//------------------------end of Yonas Z's feature and start of Yonas D's feature-------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //-------------------Yonas Z's feature------------------------
+                    case 4:
+                        cout << "4. === Daily Report ==="<<endl;
+
+                        if (totalPatrons == 0) {
+                            cout << "No patrons registered today.\n";
+                        } else {
+                            
+
+                            for (int i = 0; i < totalPatrons; ++i) {
+                                cout << "ID: " << user_id[i][0] << "\n";
+                                cout << "Name: " << user_name[i] << "\n";
+                                cout << "Sex: " << user_sex[i] << "\n";
+                                cout << "Address: " << user_address[i] << "\n";
+                                //Yeabsira's feature
+                                for (int j = 2; j < MAX_BORROWINGS+1; j++) {
+                                    cout << "Books Borrowed with id: " << user_id[i][j] <<endl;
+                                    cout<< "At Time: " << ctime(reinterpret_cast<const time_t*>(&actionTime[i][1]));
+                                    borrow_count++;
+                                }
+                                cout<<"He has taken "<<user_id[i][1]<<" books"<<endl;
+                                cout<< "-------------------\n";
+                                //End of Yeabsira's feature
+                            }
+
+                            cout << "Total Borrowings Today: " << borrow_count << " books"<<endl;
+                            //Average number of books borrowed by patrons
+                            cout<<"The average number of books borrowed per patron is: "<<borrow_count/totalPatrons<<endl;
+                            for (int i = 0; i < totalPatrons; i++) {
+                        // check if the patron has reached the borrow limit
+                        //Yeabsira's feature
+                                if (user_id[i][1] > MAX_BORROWINGS) {
+                                    cout << "Patron ID: " << user_id[i][0] << " has reached the borrow limit of " << MAX_BORROWINGS << " books\n";
+                                }
+                        //End of Yeabsira's feature
+                            }
+                        }
+                        goto menu;
+                    break;
+                    //-------------------End of Yonas Z's feature------------------------
+                    case 5:
+                    //-------------------Start of Yafet's feature------------------------
+                        cout << "5. Display Daily Borrowing Statistics\n";
+    // calculate daily statistics
+                        // count unique borrowers
+                        
+
+                        
+                        for(int i = 0; i < numBorrowings; i++) {
+                            int currentId = borrowerId[i];
+                            bool alreadyCounted = false;
+                            
+                            // check if this patron was already counted
+                            for(int j = 0; j < i; j++) {
+                                if(borrowerId[j] == currentId) {
+                                    alreadyCounted = true;
+                                    break;
+                                }
+                            }
+                            
+                            if(alreadyCounted == false) {
+                                uniqueBorrowers++;
+                            }
+                        }
+                        
+                        if(uniqueBorrowers > 0) {
+                            float average = (float)numBorrowings / uniqueBorrowers;
+                            cout << "Average books borrowed per patron: " << average << "\n";
+                        } else {
+                            cout << "No borrowings recorded today.\n";
+                        }
+                        cout<<"Patron Idcode \t Remaining borrow limit out of "<<MAX_BORROWINGS<<"\n";//highlight any patron who reched or remaining thier own borrow limit. 
+                        for(int i=0; i<5; i++){
+                        if(user_id[i][1]<MAX_BORROWINGS){
+                            cout<<"\t"<<user_id[i][0]<<"\t"<<MAX_BORROWINGS-user_id[i][1]<<"\n";
+                        }
+                        }
+                        for(int i=0; i<totalPatrons; i++){
+                            chrono::system_clock::time_point now = chrono::system_clock::now();
+                            time_t currentTime = chrono::system_clock::to_time_t(now);
+                            secondsDifference = difftime(currentTime, actionTime[i][1]);
+                            daysDifference = secondsDifference / (60 * 60 * 24); // Convert seconds to days
+
+                            cout << "ID: " << actionTime[i][0]
+                                << ", Days since action: " << daysDifference;
+
+                            if (daysDifference > dueDate) {
+                                cout << " -> Borrowed more than 10 days ago, has penalty.\n";
+                            } else {
+                                cout << " -> Borrowed within 10 days.\n";
+                            }
+                        }
+                        goto menu;
+                        break;
+                    //-------------------End of Yafet's feature------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ---------------------------Yoans Z's features----------------------
+//---------------------------------Register patron-------------------------------------------------------------
+        else if (choice == 2) {
+            if (totalPatrons >= MAX_PATRONS) {
+                cout << "Maximum patron limit reached. Cannot register more patrons.\n";
+                continue;
+            }
+            if (totalPatrons == 0) {
+                srand(time(0)); // This initializes the first random number it is called seed.
+            }
+            int id;
+            bool unique;
+            do {
+                unique = true;
+                id = (totalPatrons + 1) * 1234567 % 100000000; // Generate an 8-digit ID using a mathematical formula
+
+                // Check if ID is already assigned
+                for (int i = 0; i < totalPatrons; i++) {
+                    if (user_id[i][0] == id) {
+                        unique = false;
+                        break;
+                    }
+                }
+            } while (!unique);
+
+            
+
+            cout << "Enter your name: ";
+            cin.ignore();
+            getline(cin, name);
+
+            cout << "Enter your sex: ";
+            getline(cin, sex);
+
+            cout << "Enter your address: ";
+            getline(cin, address);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
